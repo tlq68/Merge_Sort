59af1b5f465a897a8434d5d8c0300d9dc348f517
Trevor <tlist403@gmail.com>
2021-11-06 10:26:28 -0300

    Working example

--- recursion.rb
+++ recursion.rb
@@ -1,32 +1,23 @@
 arr = [4, 5, 1, 8, 6, 2, 3, 7]
 
 def merge_sort(array)
-    # Base case: If there is only one element to sort, do nothing
-    n = array.length
-    first_half = array[0...(n/2)]
-    second_half = array[(n/2)...n]
+    length = array.length
+    left = array[0...length/2]
+    right = array[length/2..length]
     
+    return array if length < 2
 
-    if n < 2 
-        return array
-    else
-        p merge_sort(first_half)
-        p merge_sort(second_half)
-        merged = first_half << second_half
-        
+    left_side = merge_sort(left)
+    right_side = merge_sort(right)
+    sorted = []
 
+    until left_side.empty? || right_side.empty?
+        left_side[0] <= right_side[0] ? sorted << left_side.shift : sorted << right_side.shift
     end
-
-    merged.flatten
     
-    # else recursive case
-        # Sort left half [0...(n/2)]
-
-
-        # Sort right half [(n/2)...n]
-        # Merge the halves
+    sorted + left_side + right_side
 end
 
 p merge_sort(arr)
 
-# General notes. The recursion works in that we go through every element, but we have yet to figure out how to sort. 
+
